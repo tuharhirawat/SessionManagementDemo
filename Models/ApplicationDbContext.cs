2e7cbc550d670ec24068c149a78083b87d06f9cf
tuhar <tusharhirawat1918@gmail.com>
2025-06-25 11:55:26 +0100

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿//namespace SessionManagementDemo.Models
+//{
+//    public class ApplicationDbContext
+//    {
+//    }
+//}
+
+using Microsoft.EntityFrameworkCore;
+using SessionManagementDemo.Models;
+
+namespace SessionManagementDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SessionManagementDemo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
